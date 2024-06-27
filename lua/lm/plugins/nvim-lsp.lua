a651ac08f9e234820e770b8afbfbc8930766d236
luismeza8 <angel841bv@gmail.com>
2024-06-26 20:32:49 -0700

    treesitter, close auto tag and emmet added

--- lua/lm/plugins/nvim-lsp.lua
+++ lua/lm/plugins/nvim-lsp.lua
@@ -1,9 +1,11 @@
 return {
   require("mason").setup(),
   require("mason-lspconfig").setup({
-    ensure_installed = { "pyright", "lua_ls" }
+    ensure_installed = { "pyright", "lua_ls", "emmet_ls", "html" }
   }),
 
   require("lspconfig").pyright.setup {},
-  require("lspconfig").lua_ls.setup {}
+  require("lspconfig").lua_ls.setup {},
+  require("lspconfig").emmet_ls.setup {},
+  require("lspconfig").html_ls.setup {},
 }
